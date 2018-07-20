c83881619069ab79c577ce4c2c994ef3285502bc
Benjamin Kluck <kluck@sipgate.de>
2018-07-20 16:34:38 +0200

    feat: Use @clinq/bridg 3.0.0

--- clinq-pipedrive-adapter.js
+++ clinq-pipedrive-adapter.js
@@ -1,88 +1,80 @@
-const Pipedrive = require('pipedrive');
+const { promisify } = require("util");
+const Pipedrive = require("pipedrive");
 const HARD_MAX = 40000;
 const cache = [];
-const cached_keys = [];
 
-const formatNumber = (number) => {
-	let p = number.replace(/[^0-9+]/ig, "");
-	p = p.replace(/^00/, "");
-	p = p.replace(/^\+/, "");
-	p = "+" + p.replace(/^0/, "49");
-	return p
+const formatNumber = number => {
+  let p = number.replace(/[^0-9+]/gi, "");
+  p = p.replace(/^00/, "");
+  p = p.replace(/^\+/, "");
+  p = "+" + p.replace(/^0/, "49");
+  return p;
 };
 
-const keyOut = (key = '') => {
-	return "********" + key.substr(key.length - 5)
+const capitalizeFirstLetter = string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const mapResult = (input) => {
-	const data = [];
-	input.forEach(contact => {
-		if (typeof contact.name !== "undefined") {
-			const mapped = {
-				"name": contact.name
-			};
-			mapped.phoneNumbers = [];
-			contact.phone.forEach(function (numberinfo) {
-				if (numberinfo.value && numberinfo.value !== "") {
-					mapped.phoneNumbers.push(
-						{"label": numberinfo.label, "phoneNumber": formatNumber(numberinfo.value)})
-				}
-			});
-			if (mapped.phoneNumbers.length > 0) {
-				data.push(mapped)
-			}
-		}
-	});
+const mapResult = contacts =>
+  contacts
+    .filter(contact => contact.name)
+    .filter(contact => contact.phone.length > 0)
+    .map(contact => ({
+      id: String(contact.id),
+      name: contact.name,
+      company: contact.org_name || null,
+      email: contact.email.length > 0 ? contact.email[0].value : null,
+      phoneNumbers: contact.phone
+        .filter(phoneNumber => phoneNumber.value)
+        .map(phoneNumber => ({
+          label: phoneNumber.label
+            ? capitalizeFirstLetter(phoneNumber.label)
+            : null,
+          phoneNumber: formatNumber(phoneNumber.value)
+        }))
+    }));
 
-	return data
+const getAll = async (client, params) => {
+  return new Promise(resolve => {
+    client.Persons.getAll(params, function(error, data, additional) {
+      resolve({ contacts: data, info: additional });
+    });
+  });
 };
 
-const getAllPromise = (client, params) => {
-	return new Promise((resolve) => {
-		client.Persons.getAll(params, function (error, data, additional) {
-			resolve({"contacts": data, "info": additional})
-		})
-	})
-};
 const loadPage = async (offset, cache, client) => {
-	const options = {
-		start: offset,
-		limit: 100
-	};
-	return getAllPromise(client, options).then((data) => {
-		const mapped = mapResult(data.contacts).concat(cache);
-		if (data.info["pagination"]["more_items_in_collection"] && mapped.length <= HARD_MAX) {
-			offset = data.info["pagination"]["limit"] + data.info["pagination"]["start"];
-			return loadPage(offset, mapped, client)
-		} else {
-			return mapped
-		}
-	})
-};
-const loadList = async (key) => {
-	pipedriveClient = new Pipedrive.Client(key, {strictMode: true});
-	return loadPage(0, [], pipedriveClient)
+  const options = {
+    start: offset,
+    limit: 100
+  };
+  return getAll(client, options).then(data => {
+    const mapped = mapResult(data.contacts).concat(cache);
+    if (
+      data.info["pagination"]["more_items_in_collection"] &&
+      mapped.length <= HARD_MAX
+    ) {
+      offset =
+        data.info["pagination"]["limit"] + data.info["pagination"]["start"];
+      return loadPage(offset, mapped, client);
+    } else {
+      return mapped;
+    }
+  });
 };
 
-exports.getContactList = async function (key) {
-	console.log("Requesting Key: " + keyOut(key));
-	if (cached_keys.includes(key)) {
-		console.log("Responding from cache: " + keyOut(key) + " (" + cache[key].length
-			+ " contacts)");
-		loadList(key).then((apiResponse) => {
-			console.log("Filled cache: " + keyOut(key) + " (" + apiResponse.length + " contacts)");
-			cache[key] = apiResponse
-		});
-		return cache[key]
-	}
-	console.log("Preparing empty cache: " + keyOut(key));
-	cache[key] = [];
-	cached_keys.push(key);
-	loadList(key).then((apiResponse) => {
-		console.log("Filled cache: " + keyOut(key) + " (" + apiResponse.length + " contacts)");
-		cache[key] = apiResponse;
-		return apiResponse
-	});
-	return []
+exports.getContactList = async function(key) {
+  const client = new Pipedrive.Client(key, { strictMode: true });
+
+  try {
+    await promisify(client.Currencies.getAll)();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Unauthorized");
+  }
+
+  loadPage(0, [], client).then(response => {
+    cache[key] = response;
+  });
+
+  return cache[key] || [];
 };

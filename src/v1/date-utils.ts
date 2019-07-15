cc61a9802a14281411fec1ee435f0da13b3d93d1
Felix A. Gladisch <felix@felixgladisch.de>
2019-07-15 19:23:33 +0200

    fix: format duration

--- src/v1/date-utils.ts
+++ src/v1/date-utils.ts
@@ -3,10 +3,10 @@ import moment from "moment";
 export function formatDuration(ms: number): string {
 	const duration = moment.duration(ms);
 	const hours = Math.floor(duration.asHours());
-	const minutes = Math.floor(duration.minutes());
-	const minutesFormatted = hours < 10 ? `0${hours}` : hours;
-	const secondsFormatted = minutes < 10 ? `0${minutes}` : minutes;
-	return `${minutesFormatted}:${secondsFormatted}`;
+	const minutes = Math.floor(Math.max(duration.minutes(), 1));
+	const hoursFormatted = hours < 10 ? `0${hours}` : hours;
+	const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+	return `${hoursFormatted}:${minutesFormatted}`;
 }
 
 export function formatDate(time: number) {

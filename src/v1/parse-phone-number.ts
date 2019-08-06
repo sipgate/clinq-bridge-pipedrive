f5f8d16ba5b2bf630bb0c9d869150d5b37caa370
Benjamin Kluck <kluck@sipgate.de>
2019-08-06 12:57:20 +0200

    fix: Discover people by local phone number

--- src/v1/parse-phone-number.ts
+++ src/v1/parse-phone-number.ts
@@ -2,16 +2,21 @@ import { PhoneNumberFormat, PhoneNumberUtil } from "google-libphonenumber";
 
 const phoneUtil = PhoneNumberUtil.getInstance();
 
-export const normalizePhoneNumber = (phoneNumber: string) => phoneNumber.replace(/\D/g, "");
+export const normalizePhoneNumber = (phoneNumber: string) =>
+	phoneNumber.replace(/[^+\d]/g, "");
 
 export const parsePhoneNumber = (phoneNumber: string) => {
 	try {
 		return {
 			e164: normalizePhoneNumber(
-				phoneUtil.format(phoneUtil.parse(`+${phoneNumber}`), PhoneNumberFormat.INTERNATIONAL)
+				phoneUtil.format(
+					phoneUtil.parse(`+${phoneNumber}`),
+					PhoneNumberFormat.INTERNATIONAL
+				)
 			),
-			localized: normalizePhoneNumber(
-				phoneUtil.format(phoneUtil.parse(`+${phoneNumber}`), PhoneNumberFormat.NATIONAL)
+			localized: phoneUtil.format(
+				phoneUtil.parse(`+${phoneNumber}`),
+				PhoneNumberFormat.NATIONAL
 			)
 		};
 	} catch (error) {
